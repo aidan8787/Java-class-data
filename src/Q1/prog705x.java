d02fb5d1374db08a8df2a66dd51b3f3f1ec229e8
aidan <ajsiegel4418@gmail.com>
2026-09-21 16:24:39 +0000

    update

--- src/Q1/prog705x.java
+++ src/Q1/prog705x.java
@@ -10,7 +10,7 @@ public class prog705x {
         ArrayList<String[]> words= new ArrayList<>();
         try {
             var filecode = new Scanner(new File("Langdat/prog705x.txt"));
-            var file = new Scanner(new File("Langdat/prog512h.dat"));
+            var file = new Scanner(new File("Langdat/prog512h.txt"));
             while (file.hasNext()) {
                String line = file.nextLine();
                String[] temp=line.split(" ");
@@ -24,16 +24,20 @@ public class prog705x {
             String codeword="";
             while (filecode.hasNextLine()) {
                String line = filecode.nextLine();
-               int lines=Integer.parseInt(line.substring(0,2));
-               int word=Integer.parseInt(line.substring(2, 3));
-               int letter=Integer.parseInt(line.substring(3));
-               lines--;
-               word--;
-               letter--;
-               var w=poemlines.get(lines);
-               var temp=w.get(word);
-               String codeletter=temp[letter];
-                codeword+=codeletter;
+               var part1=line.substring(0,2);
+               var part2=line.substring(2, 3);
+               var part3=line.substring(3,4);
+               int lines=Integer.parseInt(part1);
+               int word=Integer.parseInt(part2);
+               int letter=Integer.parseInt(part3);
+                System.out.println("Line:"+lines);
+                System.out.println("Word:"+word);
+                System.out.println("char:"+letter);
+                System.out.println("Code:   "+line);
+               ArrayList<String[]> w=poemlines.get(lines-1);
+              String[] temp=w.get(word-1);
+               String codeletter=temp[letter-1];
+                codeword=codeword+codeletter;
                System.out.println("Code:   "+line+"   Letter: "+codeletter);
             }
             System.out.println("Code Word: "+codeword);
