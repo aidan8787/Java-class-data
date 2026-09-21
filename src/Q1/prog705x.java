15e52c04ddca084349c2e41ea02068bba71ed475
aidan <ajsiegel4418@gmail.com>
2026-09-21 15:31:24 +0000

    update

--- src/Q1/prog705x.java
+++ src/Q1/prog705x.java
@@ -6,22 +6,32 @@ import java.util.Scanner;
 
 public class prog705x {
     public static void main(String[] args) {
-        ArrayList<String[]> poemlines= new ArrayList<>();
+        ArrayList<ArrayList<String[]>> poemlines= new ArrayList<>();
+        ArrayList<String[]> words= new ArrayList<>();
         try {
-            var file = new Scanner(new File("Langdat/prog705x.txt"));
-            var filecode = new Scanner(new File("Langdat/prog512h.dat"));
+            var filecode = new Scanner(new File("Langdat/prog705x.txt"));
+            var file = new Scanner(new File("Langdat/prog512h.dat"));
             while (file.hasNext()) {
                String line = file.nextLine();
-               String[] chars=line.split("");
-               poemlines.add(chars);
+               String[] temp=line.split(" ");
+               for(var word:temp){
+                String[] chars=word.split("");
+                words.add(chars);
+               }
+               poemlines.add(words);
             }
             file.close();
             String codeword="";
-            while (filecode.hasNext()) {
+            while (filecode.hasNextLine()) {
                String line = filecode.nextLine();
-               int lines=Integer.parseInt(line.substring(1,3));
-               int letter=Integer.parseInt(line.substring(3,line.length()));
-               var temp=poemlines.get(lines);
+               int lines=Integer.parseInt(line.substring(0,2));
+               int word=Integer.parseInt(line.substring(2, 3));
+               int letter=Integer.parseInt(line.substring(3));
+               lines--;
+               word--;
+               letter--;
+               var w=poemlines.get(lines);
+               var temp=w.get(word);
                String codeletter=temp[letter];
                 codeword+=codeletter;
                System.out.println("Code:   "+line+"   Letter: "+codeletter);
